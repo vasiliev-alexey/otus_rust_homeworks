478c096b8825d62b3a8cd18a0803e350e52e1d0c
vasiliev-alexey <alex_v81@MAIL.RU>
2023-07-06 15:55:34 +0300

    hw6 start

---
+++ hw4/src/get_n_element_from_end.rs
@@ -0,0 +1,29 @@
+// 3. Принимает слайс и число N. Возвращает ссылку на N-ый элемент слайса с конца.
+fn get_element_from_end<T>(values: &mut [T], n: usize) -> &T {
+    if n >= values.len() {
+        panic!("Index out of bounds");
+    }
+    &values[values.len() - 1 - n]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    struct Mock {
+        test: usize,
+    }
+    #[test]
+    fn test_get_element_from_end() {
+        let mut vec = vec![Mock { test: 0 }, Mock { test: 1 }];
+        let res = get_element_from_end(&mut vec[0..2], 1);
+        assert_eq!(res.test, 0);
+        let res = get_element_from_end(&mut vec[0..2], 0);
+        assert_eq!(res.test, 1);
+    }
+    #[test]
+    #[should_panic]
+    fn test_get_element_bigger_index() {
+        let mut vec = vec![Mock { test: 0 }, Mock { test: 1 }];
+        get_element_from_end(&mut vec[0..2], 10);
+    }
+}
---
+++ hw4/src/lib.rs
@@ -0,0 +1,3 @@
+mod get_element_from_pair;
+mod get_n_element_from_end;
+mod get_n_element_from_slice;
