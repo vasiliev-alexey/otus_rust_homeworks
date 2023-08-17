208181fa3c4110fa3691efdc3ff607b29050d486
vasiliev-alexey <alex_v81@MAIL.RU>
2023-08-17 18:57:56 +0300

    Компилятор (#11)

---
+++ hw7/src/main.rs
@@ -0,0 +1,84 @@
+fn main() {
+    let arr = [2, 4, 8, 16];
+
+    let mut n = 2;
+    let nth = nth_item(&arr, &n);
+    let increased = increased_by_first_item(&arr, &mut n);
+
+    let value = {
+        let values = TwoValues::new(&arr[3], increased);
+
+        assert_eq!(*values.get_first(), 16);
+
+        values.get_second()
+    };
+
+    assert_eq!(*value, 4);
+    assert_eq!(*nth, 8);
+}
+
+fn nth_item<'a>(data: &'a [usize], n: &usize) -> &'a usize {
+    &data[*n]
+}
+
+fn increased_by_first_item<'a>(data: &[usize], n: &'a mut usize) -> &'a mut usize {
+    *n += data[0];
+    n
+}
+
+struct TwoValues<'a, 'b> {
+    first: &'a usize,
+    second: &'b usize,
+}
+
+impl<'a, 'b> TwoValues<'a, 'b> {
+    pub fn new(first: &'a usize, second: &'b usize) -> Self {
+        Self { first, second }
+    }
+
+    pub fn get_first(&self) -> &'a usize {
+        self.first
+    }
+
+    pub fn get_second(&self) -> &'b usize {
+        self.second
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+
+    #[test]
+    fn test_two_values_new() {
+        let values = TwoValues::new(&1, &2);
+        assert_eq!(&1, values.get_first());
+        assert_eq!(&2, values.get_second());
+    }
+
+    #[test]
+    fn test_increased_by_first_item() {
+        let arr = [500, 1, 2, 3];
+        let mut n = 100_000;
+        increased_by_first_item(&arr, &mut n);
+        assert_eq!(n, 100_500);
+    }
+
+    #[test]
+    fn test_nth_item() {
+        let arr = [2, 4, 8, 16];
+        assert_eq!(*nth_item(&arr, &2), 8_usize);
+    }
+
+    #[test]
+    fn test_from_pr() {
+        let c;
+        let b = 4;
+        {
+            let a = 3;
+            let values = TwoValues::new(&a, &b);
+            c = values.get_second();
+        }
+        println!("{}", c);
+    }
+}
