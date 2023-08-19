32e56cad907e1f9da6110ca545e09798ba1e6086
vasiliev-alexey <alex_v81@mail.ru>
2023-08-19 23:29:28 +0300

    final solution

--- hw8/src/task2.rs
+++ hw8/src/task2.rs
@@ -1,43 +1,22 @@
-#[allow(dead_code)]
-fn f() -> i32 {
-    println!("fo call");
-    1
-}
-fn fo() -> i32 {
-    println!("fo0 call");
-    22222
-}
-fn foo() -> i32 {
-    println!("fooo call");
-    3
-}
-fn fooo() -> i32 {
-    println!("fooo call");
-    4
-}
+use task2::gen_dummy_function;
 
-fn test() -> i32 {
-    println!("fooo call");
-    -1
-}
-
-// fn res() -> (i32, i32) {
-//     (fo(), fooo())
-// }
-
+gen_dummy_function!(0);
+gen_dummy_function!(1);
+gen_dummy_function!(2);
+gen_dummy_function!(3);
+gen_dummy_function!(4);
+gen_dummy_function!(9);
 #[cfg(test)]
 mod tests {
     use super::*;
-    use task2::macro2;
+    use task2::even_len_name_func_invoke;
 
     #[test]
     fn test_2() {
-        // let res22 = macro2!("foo", "fo", "fooo");
-        let (fo_result, fooo_result) = macro2!("fo", "foo", "fooo");
-        // println!("res: {}", res22.0);
-        println!("res: {}", fo_result);
-        println!("res: {}", fooo_result);
-
-        //   let (fo_result, fooo_result) = my_macro!(""fo"", ""foo"", ""fooo"");
+        let (fo_result, fooo_result, foooooooooo_result) =
+            even_len_name_func_invoke!("fo", "foo", "fooo", "fooooooooo");
+        assert_eq!(fo_result, 2);
+        assert_eq!(fooo_result, 4);
+        assert_eq!(foooooooooo_result, 10);
     }
 }
--- hw8/src/task2/src/lib.rs
+++ hw8/src/task2/src/lib.rs
@@ -1,64 +1,23 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
-use std::vec;
-use syn::parse_quote;
-use syn::Stmt::Expr;
 
 #[proc_macro]
-pub fn macro2(input: TokenStream) -> TokenStream {
-    println!("R-------------------------------------: {}", input);
+pub fn even_len_name_func_invoke(input: TokenStream) -> TokenStream {
+    let even_func_name_idents = input
+        .into_iter()
+        .map(|name| name.to_string().replace('\"', ""))
+        .filter(|x| x.to_string().len() % 2 == 0)
+        .map(|name| Ident::new(&name, Span::call_site()))
+        .collect::<Vec<_>>();
+    TokenStream::from(quote::quote! {
+    (  #(#even_func_name_idents() , )* )
+    })
+}
 
-    let input = input.into_iter().collect::<Vec<_>>();
-    //let name = input[0].to_string().replace("\"", "");
-
-    // let ident = Ident::new(&*name, Span::call_site());
-    // for x in input {
-    //     let name = x.to_string().replace("\"", "");
-    //     println!("name:{}", name);
-    //     let ident = Ident::new(&*name, Span::call_site());
-    //
-    // }
-    // let mut field_constructors =
-    //     syn::punctuated::Punctuated::<syn::Ident, syn::Token![,]>::default();
-
-    let mut field_constructors = vec![];
-    println!("R: {}", 1);
-    for arg in input.iter().filter(|x| x.to_string().len() % 2 == 0) {
-        let name = arg.to_string().replace("\"", "");
-        println!("R1: {}", arg);
-        let ident = Ident::new(&*(name), Span::call_site());
-
-        field_constructors.push(ident)
-        //  let mut res: i32;
-
-        // #ident();
-        // println!("R2: {}", res);
-
-        //  #ident()
-    }
-    println!("R: {}", 2);
-    //let r = parsed.value();
-    //  let ident = Ident::new(&*r, Span::call_site());
-    //  field_constructors.push(1);
-    // field_constructors.push(2);
-    // let _st: proc_macro2::TokenStream = parse_quote! {
-    //    //    println!("{} started");
-    //      //  ( #ident() , #ident() , #ident())
-    //
-    //    // (                #ident()    ,#ident()           )
-    // (  1,2,3)
-    //  // (#ident(),2,3)
-    //
-    //     //   println!("{} finished");
-    //     };
-    // _st.into()
-    let str11 = "test()";
-    println!("R: {}", 33);
-    //     // (#field_constructors(),)
-    //   let nums: Vec<_> = field_constructors.iter().collect();
-    let ret = quote::quote! {
-    (  #(#field_constructors() , )* )
-
-    };
-    TokenStream::from(ret)
+#[proc_macro]
+pub fn gen_dummy_function(item: TokenStream) -> TokenStream {
+    let length = item.to_string().parse().unwrap();
+    let func_name = "f".to_string() + &*"o".to_string().repeat(length);
+    let func_src = format!("fn {func_name}() -> u32 {{ {} }}", length + 1);
+    func_src.parse().unwrap()
 }
