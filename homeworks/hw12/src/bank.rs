511a82319c59623971f2fed2cf9e186ee69daada
vasiliev-alexey <alex_v81@mail.ru>
2023-09-29 18:39:13 +0300

    add thiserror for error handling

--- homeworks/hw12/src/bank.rs
+++ homeworks/hw12/src/bank.rs
@@ -6,6 +6,7 @@
 use nanoid::nanoid;
 use std::cell::RefCell;
 use std::collections::{HashMap, VecDeque};
+use thiserror::Error;
 
 type Money = f64;
 
@@ -35,6 +36,51 @@ enum OperationType {
     Transfer,
 }
 
+#[derive(Debug, Error, PartialEq)]
+#[error("Account already exists")]
+pub struct AccountDuplicationError {
+    account: String,
+}
+
+#[derive(Debug, Error, PartialEq)]
+#[error("Account does not exist")]
+pub struct AccountNotFoundError {
+    account: String,
+}
+#[derive(Debug, Error, PartialEq)]
+#[error("Cannot transfer to the same account")]
+pub struct SomeAccountTransferError {
+    account: String,
+}
+
+#[derive(Debug, Error, PartialEq)]
+#[error("Amount must be positive")]
+pub struct AmountNegativeError {
+    account: String,
+    amount: Money,
+}
+#[derive(Debug, Error, PartialEq)]
+#[error("Insufficient funds")]
+pub struct InsufficientFundsError {
+    account: String,
+    amount: Money,
+    balance: Money,
+}
+
+#[derive(Debug, Error, PartialEq)]
+pub enum BankError {
+    #[error("Account already exists")]
+    AccountDuplication(#[from] AccountDuplicationError),
+    #[error("Amount must be positive")]
+    AmountNegative(#[from] AmountNegativeError),
+    #[error("Account does not exist")]
+    AccountNotFound(#[from] AccountNotFoundError),
+    #[error("Insufficient funds")]
+    InsufficientFunds(#[from] InsufficientFundsError),
+    #[error("Cannot transfer to the same account")]
+    SomeAccountTransfer(#[from] SomeAccountTransferError),
+}
+
 fn get_next_id() -> String {
     nanoid!(10)
 }
@@ -53,13 +99,17 @@ impl Bank {
     /// * `account` - The code of the account to create.
     ///
     /// # Errors
+    /// AccountDuplicationError
     ///
     /// Returns an error if an account with the same name already exists in the bank.
     ///
     /// ```
-    pub fn create_account(&mut self, account: &str) -> Result<(), &'static str> {
+    pub fn create_account(&mut self, account: &str) -> Result<(), BankError> {
         if self.accounts.contains_key(account) {
-            Err("Account already exists")
+            Err(AccountDuplicationError {
+                account: account.to_owned(),
+            }
+            .into())
         } else {
             self.accounts
                 .insert(account.to_owned(), RefCell::from(MONEY_ZERO));
@@ -79,12 +129,20 @@ impl Bank {
     /// # Arguments
     ///
     /// * `amount` - The amount to deposit into the account.
+    /// # Errors
+    /// AmountNegativeError
+    /// AccountNotFoundError
     ///
     /// ```
-    pub fn deposit(&mut self, account: &str, amount: Money) -> Result<(), &'static str> {
+    pub fn deposit(&mut self, account: &str, amount: Money) -> Result<(), BankError> {
         if let Some(balance) = self.accounts.get_mut(account) {
             if amount <= Money::default() {
-                Err("Amount must be positive")
+                // Err("Amount must be positive")
+                Err(AmountNegativeError {
+                    account: account.to_owned(),
+                    amount,
+                }
+                .into())
             } else {
                 *balance.get_mut() += amount;
                 let operation = Operation {
@@ -98,7 +156,10 @@ impl Bank {
                 Ok(())
             }
         } else {
-            Err("Account does not exist")
+            Err(AccountNotFoundError {
+                account: account.to_owned(),
+            }
+            .into())
         }
     }
     /// Withdraws the specified amount from the account.
@@ -108,17 +169,30 @@ impl Bank {
     /// * `amount` - The amount to withdraw from the account.
     ///
     /// # Errors
+    /// AmountNegativeError
+    /// AccountNotFoundError
+    /// InsufficientFundsError
     ///
     /// Returns an error if the account balance is insufficient to cover the withdrawal amount.
     ///
     /// ```
 
-    pub fn withdraw(&mut self, account: &str, amount: Money) -> Result<(), &'static str> {
+    pub fn withdraw(&mut self, account: &str, amount: Money) -> Result<(), BankError> {
         if let Some(balance) = self.accounts.get_mut(account) {
             if amount <= Money::default() {
-                Err("Amount must be positive")
+                Err(AmountNegativeError {
+                    account: account.to_owned(),
+                    amount,
+                }
+                .into())
             } else if *balance < RefCell::from(amount) {
-                Err("Insufficient funds")
+                //  Err("Insufficient funds")
+                Err(InsufficientFundsError {
+                    amount,
+                    account: account.to_owned(),
+                    balance: *balance.borrow(),
+                }
+                .into())
             } else {
                 *balance.get_mut() -= amount;
                 let operation = Operation {
@@ -132,7 +206,10 @@ impl Bank {
                 Ok(())
             }
         } else {
-            Err("Account does not exist")
+            Err(AccountNotFoundError {
+                account: account.to_owned(),
+            }
+            .into())
         }
     }
     /// Transfers the specified amount from one account to another.
@@ -144,6 +221,10 @@ impl Bank {
     /// * `amount` - The amount to transfer.
     ///
     /// # Errors
+    /// AmountNegativeError
+    /// AccountNotFoundError
+    /// InsufficientFundsError
+    /// SomeAccountTransferError
     ///
     /// Returns an error if either the sender or receiver account does not exist, or if
     /// the sender account does not have sufficient balance to cover the transfer amount.
@@ -154,17 +235,29 @@ impl Bank {
         sender_account: &str,
         receiver_account: &str,
         amount: Money,
-    ) -> Result<(), &'static str> {
+    ) -> Result<(), BankError> {
         if sender_account == receiver_account {
-            return Err("Cannot transfer to the same account");
+            return Err(SomeAccountTransferError {
+                account: sender_account.to_owned(),
+            }
+            .into());
         }
 
         if let Some(sender_balance) = self.accounts.get(sender_account) {
             if let Some(receiver_balance) = self.accounts.get(receiver_account) {
                 if amount <= MONEY_ZERO {
-                    Err("Amount must be positive")
+                    Err(AmountNegativeError {
+                        amount,
+                        account: sender_account.to_owned(),
+                    }
+                    .into())
                 } else if *sender_balance < RefCell::from(amount) {
-                    Err("Insufficient funds")
+                    Err(InsufficientFundsError {
+                        amount,
+                        account: sender_account.to_owned(),
+                        balance: *sender_balance.borrow(),
+                    }
+                    .into())
                 } else {
                     *sender_balance.borrow_mut() -= amount;
                     *receiver_balance.borrow_mut() += amount;
@@ -181,10 +274,16 @@ impl Bank {
                     Ok(())
                 }
             } else {
-                Err("Receiver account does not exist")
+                Err(AccountNotFoundError {
+                    account: receiver_account.to_owned(),
+                }
+                .into())
             }
         } else {
-            Err("Sender account does not exist")
+            Err(AccountNotFoundError {
+                account: sender_account.to_owned(),
+            }
+            .into())
         }
     }
     /// Returns the current balance of the account.
@@ -280,7 +379,14 @@ mod tests {
         let mut bank = Bank::new();
         assert!(bank.create_account("Alice").is_ok());
         assert!(bank.create_account("Bob").is_ok());
-        assert_eq!(bank.create_account("Alice"), Err("Account already exists"));
+        let duplicate_account = bank.create_account("Alice");
+        assert!(duplicate_account.is_err());
+        assert_eq!(
+            duplicate_account.unwrap_err(),
+            BankError::AccountDuplication(AccountDuplicationError {
+                account: "Alice".to_string()
+            })
+        );
     }
 
     #[test]
@@ -289,7 +395,16 @@ mod tests {
         bank.create_account("Alice").unwrap();
         assert!(bank.deposit("Alice", 100.0).is_ok());
         assert_eq!(bank.get_balance("Alice"), Some(100.0));
-        assert_eq!(bank.deposit("Alice", -50.0), Err("Amount must be positive"));
+        let res = bank.deposit("Alice", -50.0);
+        assert!(res.is_err());
+
+        assert_eq!(
+            res.unwrap_err(),
+            BankError::AmountNegative(AmountNegativeError {
+                account: "Alice".to_string(),
+                amount: -50.0
+            })
+        );
     }
 
     #[test]
@@ -300,12 +415,27 @@ mod tests {
 
         assert!(bank.withdraw("Alice", 50.0).is_ok());
         assert_eq!(bank.get_balance("Alice"), Some(50.0));
-        assert_eq!(
-            bank.withdraw("Alice", -30.0),
-            Err("Amount must be positive")
-        );
+        let res = bank.withdraw("Alice", -30.0);
+        assert!(res.is_err());
 
-        assert_eq!(bank.withdraw("Alice", 100.0), Err("Insufficient funds"));
+        assert_eq!(
+            res.unwrap_err(),
+            BankError::AmountNegative(AmountNegativeError {
+                account: "Alice".to_string(),
+                amount: -30.0
+            })
+        );
+        let res = bank.withdraw("Alice", 100.0);
+        assert!(res.is_err());
+
+        assert_eq!(
+            res.unwrap_err(),
+            BankError::InsufficientFunds(InsufficientFundsError {
+                account: "Alice".to_string(),
+                balance: 50.0,
+                amount: 100.0
+            })
+        );
     }
 
     #[test]
@@ -319,29 +449,56 @@ mod tests {
         assert_eq!(bank.get_balance("Alice"), Some(50.0));
         assert_eq!(bank.get_balance("Bob"), Some(50.0));
 
-        assert_eq!(
-            bank.transfer("Alice", "Bob", -20.0),
-            Err("Amount must be positive")
-        );
+        let res = bank.transfer("Alice", "Bob", -30.0);
+        assert!(res.is_err());
 
         assert_eq!(
-            bank.transfer("Alice", "Bob", 100.0),
-            Err("Insufficient funds")
+            res.unwrap_err(),
+            AmountNegativeError {
+                amount: -30.0,
+                account: "Alice".to_string(),
+            }
+            .into()
         );
+        let res = bank.transfer("Alice", "Bob", 100.0);
+        assert!(res.is_err());
 
         assert_eq!(
-            bank.transfer("Alice", "Alice", 20.0),
-            Err("Cannot transfer to the same account")
+            res.unwrap_err(),
+            InsufficientFundsError {
+                amount: 100.0,
+                account: "Alice".to_string(),
+                balance: 50.0
+            }
+            .into()
         );
+        let res = bank.transfer("Alice", "Alice", 20.0);
+        assert!(res.is_err());
 
         assert_eq!(
-            bank.transfer("Eve", "Bob", 10.0),
-            Err("Sender account does not exist")
+            res.unwrap_err(),
+            SomeAccountTransferError {
+                account: "Alice".to_string()
+            }
+            .into()
         );
-
+        let res = bank.transfer("Eve", "Bob", 10.0);
+        assert!(res.is_err());
         assert_eq!(
-            bank.transfer("Alice", "Eve", 10.0),
-            Err("Receiver account does not exist")
+            res.unwrap_err(),
+            AccountNotFoundError {
+                account: "Eve".to_string()
+            }
+            .into()
+        );
+        let res = bank.transfer("Alice", "Eve", 10.0);
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err(),
+            AccountNotFoundError {
+                account: "Eve".to_string()
+            }
+            .into()
         );
     }
 
